635c643b9a5a774c8a79a1d494cc942342e78180
Bogdan <bogd.moskv@gmail.com>
2023-07-15 11:36:06 +0300

    Add HomeView, MainView, Styles, basic architecture

---
+++ CryptocurrenciesProject/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace CryptocurrenciesProject.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        public ViewModelBase CurrentChildView
+        {
+            get => _currentChildView;
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        public ICommand ShowHomeViewCommand
+        {
+            get;
+        }
+
+        public MainViewModel()
+        {
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+        }
+
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Головна сторінка";
+            Icon = IconChar.Home;
+        }
+    }
+}
